25305d88db6025d5e5e55583f2628ec31d705637
hkanoute <Abdelhamidou4@gmail.com>
2020-03-13 19:10:28 +0100

    javaScript pour la recgercge

--- footer.php
+++ footer.php
@@ -1,9 +1,27 @@
 <hr />
 <footer class="footer mt-auto py-3">
-  <div class="container">
-    <span class="text-muted">SIO SLAM MyWebApp &copy; 2020</span>
-  </div>
+    <div class="container">
+        <span class="text-muted">SIO SLAM MyWebApp &copy; 2020</span>
+    </div>
 </footer>
+<script>
+    function myFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+</script>
 
-  </body>
+</body>
 </html>
